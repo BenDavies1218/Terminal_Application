adbb2b069b9b39c190134271f776ab2e0c173bb7
BenDavies1218 <14972@coderacademy.edu.au>
2023-12-12 10:49:17 +1000

    csv log for shuffle cases

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 import random
+import csv
+
+file = "gamelog.csv"
 
 def game_menu():
     print("Welcome to Deal or No Deal Will you be a millionaire \n press any button to start")
@@ -8,10 +11,16 @@ def shuffler(cases):
     values = list(cases.values())
     shuffle_list = random.sample(values, len(values))
     shuffle_cases = dict(zip(cases.keys(), shuffle_list))
+    with open(file, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([shuffle_cases, "True"])
     return shuffle_cases
 
-def select_case(cases):
+def select_case(cases_left):
     pass
+    # print cases left to user
+    # user will input a number between and including 1-22
+    # will remove 
 
 def game():
     pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from functions import game_menu, shuffler, game, banker_offer, game_finish, double_or_nothing
+from functions import game_menu, shuffler, select_case, game, banker_offer, game_finish, double_or_nothing
+import csv
 
 file = "gamelog.csv"
 
@@ -8,7 +9,7 @@ try:
 
 except FileNotFoundError:
     gamelog = open(file, "w")
-    gamelog.write("Deal or No Deal")
+    gamelog.write("Deal or No Deal\n")
     gamelog.close()
 
 cases = {
@@ -35,8 +36,8 @@ cases = {
         21: 150000,
         22: 200000
     }
-cases_left = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+cases_left = ["|", "1", "|", 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 
 game_menu()
 shuffled_cases = shuffler(cases)
-print(shuffled_cases)
+select_case(cases_left)
